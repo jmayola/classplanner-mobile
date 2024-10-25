840bf58efdcb0b66736cee40cfd0e253450924ad
mayola <mayola@marketpsychdata.com>
2024-10-25 01:14:51 -0300

    first commit, setted up nativewind && expo-router

---
+++ app/(clases)/index.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { colorScheme } from 'nativewind'
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center dark:text-white text-black dark:bg-gray-900 bg-gray-200">
+      <Text className="text-xl border-b-2 border-slate-700">Clases</Text>
+      <StatusBar style='auto' />
+    </View>
+  )
+}
+
+export default index
